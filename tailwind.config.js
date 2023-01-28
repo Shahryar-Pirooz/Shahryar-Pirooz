b02ad065e31a671443d1d0cb7812f10df45d0626
DevEss <dev.shahryar@gmail.com>
2023-01-28 22:35:45 +0330

    custom color and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
+    colors: {
+      background: '#16161A',
+      'background-shade': '#242629',
+      accent: '#2CB67D',
+      primary: '#7F5AF0',
+      body: '#94A1B2',
+      head: '#FFFFFE',
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
