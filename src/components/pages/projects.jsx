6fbbcdff072438e11eb3ef066269c958bb27d624
DevEss <dev.shahryar@gmail.com>
2023-01-29 21:22:58 +0330

    app connected to github by axios

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,7 +1,26 @@
-export default function Projects() {
+import axios from 'axios'
+import React from 'react'
+export default function Projects(data) {
+  // console.log(data)
+  const w = window.innerWidth / 3 - 30
   return (
-    <div className="bg-background flex flex-row flex-nowrap w-screen h-screen">
-      <p className="text-head">PROJECTS</p>
+    <div className="px-3 w-screen h-screen grid grid-cols-3 gap-10 content-start justify-items-center place-items-center overflow-auto">
+      {data.map((project) => {
+        return (
+          <a href={project.url}>
+            <div
+              key={project.id}
+              className="py-2 px-5 min-w-full  rounded-lg text-center bg-background-shade"
+            >
+              <p className="text-xl text-head">{project.name}</p>
+              <p className="text-base text-body">{project.description}</p>
+              <div>
+                <p className="text-sm text-primary">{project.language}</p>
+              </div>
+            </div>
+          </a>
+        )
+      })}
     </div>
   )
 }
