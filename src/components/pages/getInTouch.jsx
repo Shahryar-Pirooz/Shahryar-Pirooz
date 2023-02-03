83e81d410ad4b7119d115eb8fc69ff78565759a8
DevEss <dev.shahryar@gmail.com>
2023-02-03 11:12:37 +0330

    a bug fixed

--- src/components/pages/getInTouch.jsx
+++ src/components/pages/getInTouch.jsx
@@ -1,22 +1,20 @@
 export default function GetInTouch() {
   return (
-    <div className="flex flex-col flex-nowrap items-center justify-center p-3">
-      <div>
-        <h3 className="text-center text-3xl font-bold text-head">
-          Say Somthing ...
-        </h3>
-        <p className="mt-2 text-center text-lg text-body">
-          If you want to tell me something, ask a question, or hire me, fill out
-          this form and I will get in touch with you.
-          <br />( You can use markdown )
-        </p>
-        <a
-          href="mailto:dev.shahryar@gmail.com"
-          className="my-10 rounded-lg bg-primary px-10 py-5 text-center text-head "
-        >
-          GetInTouch
-        </a>
-      </div>
+    <div className="flex h-screen w-screen flex-col flex-nowrap items-center justify-center">
+      <h3 className="text-center text-3xl font-bold text-head">
+        Say Somthing ...
+      </h3>
+      <p className="mt-2 text-center text-lg text-body">
+        If you want to tell me something, ask a question, or hire me, fill out
+        this form and I will get in touch with you.
+        <br />( You can use markdown )
+      </p>
+      <a
+        href="mailto:dev.shahryar@gmail.com"
+        className="my-10 rounded-lg bg-primary px-10 py-5 text-center text-head "
+      >
+        GetInTouch
+      </a>
     </div>
-  );
+  )
 }
