c2c6d788421ecd9113ee2a14c054ef20a5533b60
DevEss <dev.shahryar@gmail.com>
2023-02-15 18:39:38 +0330

    modified:   src/components/body.jsx new file:   src/components/pages/index.js

---
+++ src/components/pages/index.js
@@ -0,0 +1,7 @@
+import Hero from './hero'
+import Skills from './skills'
+import Projects from './projects'
+import AboutMe from './aboutMe'
+import GetInTouch from './getInTouch'
+
+export { Hero, Skills, Projects, AboutMe, GetInTouch }
