506762cd9786ab93a837f72910f9cc8c1a2e9b11
DevEss <dev.shahryar@gmail.com>
2023-02-16 11:56:44 +0330

    state page removed

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Body, NavBar } from './components'
 
 export default function App() {
-  const [pageState, setPageState] = useState('Home')
-  // handlers
-  const handlePage = (target) => setPageState(target)
-
   return (
     <div>
-      <NavBar state={pageState} handler={handlePage} />
-      <Body state={pageState} />
+      <NavBar />
+      <Body />
     </div>
   )
 }
