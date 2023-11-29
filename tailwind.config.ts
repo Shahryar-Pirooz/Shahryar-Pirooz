8a875fc545b217104fa9032a07c3f361eb6688f4
DevEss <dev.shahryar@gmail.com>
2023-11-29 10:45:55 +0330

    conf: set initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				accent: 'var(--accent-color)',
+				background: 'var(--background-color)',
+				primary: 'var(--primary-color)',
+				secondary: 'var(--secondary-color)',
+				text: 'var(--text-color)',
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
