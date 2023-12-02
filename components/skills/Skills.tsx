bc63252c6dcb9103bdcad65ef02a7a6377a90317
DevEss <dev.shahryar@gmail.com>
2023-12-02 13:09:26 +0330

    complete skill section

---
+++ components/skills/Skills.tsx
@@ -0,0 +1,63 @@
+import { CodeSVG, MusicSVG, ToolSVG } from '@/public'
+
+import Card from '../card/Card'
+
+export default function Skills() {
+	return (
+		<section className='h-fit rounded-lg bg-secondary p-4'>
+			<div className='flex flex-row md:justify-start'>
+				<h3 className='w-full text-center text-xl font-bold md:text-start'>
+					Skills & Specializations
+				</h3>
+			</div>
+			<div className='flex flex-col justify-around py-4 md:flex-row'>
+				{[
+					{
+						title: 'Development',
+						icon: <CodeSVG />,
+						context:
+							'Front-End Web Development\nJavaScript, TypeScript, React\nTransitioning to MERN Stack\nMobile App Development (Android)\nVersatility in Java, Dart, Python',
+					},
+					{
+						title: 'Technical Versatility',
+						icon: <ToolSVG />,
+						context:
+							'Embracing Open-Source Technologies\nProficiency in Linux\nInterest in Cloud Computing\nDevOps Knowledge',
+					},
+					{
+						title: 'Creative Pursuits',
+						icon: <MusicSVG />,
+						context:
+							'Passion for Music\nLove for Literature\nExploring New Rhythms and Stories\nIntersection of Technology and Creativity',
+					},
+				].map((item) => {
+					return (
+						<div className='basis-1/3'>
+							<Card>
+								<div className='flex h-52 w-full flex-col items-center justify-start space-y-4 py-4 text-center'>
+									<div className='basis-1/3 fill-text'>
+										{item.icon}
+									</div>
+									<div className='basis-1/3 text-lg font-bold'>
+										{item.title}
+									</div>
+									<div className='basis-1/3 justify-self-end text-start text-xs'>
+										<ul className='list-disc'>
+											{item.context
+												.split('\n')
+												.map((line) => (
+													<li>{line}</li>
+												))}
+										</ul>
+									</div>
+								</div>
+							</Card>
+						</div>
+					)
+				})}
+				<div className=''></div>
+				<div className=''></div>
+			</div>
+		</section>
+	)
+}
