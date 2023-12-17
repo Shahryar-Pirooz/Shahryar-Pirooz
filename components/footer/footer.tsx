c6d2603faddc52304e495a88ca6831d059f65021
DevEss <dev.shahryar@gmail.com>
2023-12-17 10:10:52 +0330

    fix footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,8 +2,15 @@ import { Linkedin, Telegram } from '@/public'
 
 export default function Footer() {
 	return (
-		<footer className='flex h-full w-full flex-row items-center justify-center bg-primary px-4 py-8 text-text '>
-			<div className='flex w-fit flex-row justify-around space-x-2 px-4'>
+		<footer className='flex h-full w-full flex-col space-y-3 bg-primary py-8'>
+			<div className='flex w-full flex-row justify-center px-4'>
+				<span className='text-center'>
+					AI-powered hero image by segmind
+					<br />
+					design and code by me.
+				</span>
+			</div>
+			<div className='flex flex-row items-center justify-center space-x-3'>
 				<a
 					href='https://www.linkedin.com/in/shahryar-pirooz/'
 					target='_blank'
@@ -13,13 +20,6 @@ export default function Footer() {
 				<a href='https://t.me/Dev_3ss/' target='_blank'>
 					<Telegram />
 				</a>
-			</div>
-			<div className='flex w-full flex-row justify-center px-4'>
-				<span className='text-center'>
-					AI-powered hero image by segmind
-					<br />
-					design and code by me.
-				</span>
 			</div>
 		</footer>
 	)
